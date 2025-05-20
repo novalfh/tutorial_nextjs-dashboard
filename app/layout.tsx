349c8e5dd15fbd61c204df02a4eab1466d6f4246
novalfh <annaufal1521@gmail.com>
2025-05-20 21:38:19 +0700

    zerotwo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import '@/app/ui/global.css'
+import {inter} from "@/app/ui/font";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // let count = 7;
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+      {/* <h1>{count}</h1> */}
+      {/* <div className='w-full bg-mygreen-800 h-[4rem]'></div> */}
+      {/* <div className='w-full bg-mygreen-100 h-[4rem]'></div> */}
+      {children}
+      </body>
     </html>
   );
 }
